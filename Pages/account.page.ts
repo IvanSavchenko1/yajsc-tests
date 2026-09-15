9943a15fdd595c85e010c9363a0deb15aed96eb7
Ivan Savchenko <ivan.savchenko@gmail.com>
2026-09-15 20:40:24 +0300

    unit11 homework

---
+++ Pages/account.page.ts
@@ -0,0 +1,15 @@
+import { Locator, Page } from "@playwright/test";
+import { HeaderFragment } from "./Fragments/headerFragment.page";
+
+export class AccountPage {
+    page: Page;
+    header: HeaderFragment;
+    pageTitle: Locator;
+    
+    constructor(page: Page) {
+        this.page = page;
+        this.header = new HeaderFragment(page);
+        
+        this.pageTitle = this.page.getByTestId('page-title');
+    }
+}
